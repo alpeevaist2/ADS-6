48786b126f50d4299a231f1dd535eef4ccd60b4a
alpeevaist2 <62651118+alpeevaist2@users.noreply.github.com>
2020-05-30 22:54:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,37 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+using namespace std;
 
 BST<std::string> makeTree(char* filename)
 {
 
+} 
+BST<string> makeTree(char* filename) {
+BST<string> tree;
+    ifstream file(filename);
+    string str,word;
+    while (!file.eof()) {
+        word.clear();
+        file >> str;
+        bool flag = 0;
+        for (auto &letter : str) {
+            if (isalpha(letter))
+            {
+                flag = 1;
+                letter=tolower(letter);
+                word += letter;
+            }
+            else if (flag)
+            {
+                tree.add(word);
+                word.clear();
+                flag = 0;
+            }
+        }
+        if (!word.empty()) {
+            tree.add(word);
+        }
+    }
+    return tree;
 }
